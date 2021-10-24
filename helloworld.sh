46f4e1f3f5f88a3c84a8b69e73f30de9a116d9e1
anvishnu1984 <51805537+anvishnu1984@users.noreply.github.com>
2021-10-24 16:59:47 +1100

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
 echo "Good Morning"
+if [ $1 == "dev" ]
+then
+    echo " dev environment"
+fi
+  
+#Check whether they are not equal
+if [ $1 == "test" ]
+then
+    echo "test environment"
+fi
+
+if [ $1 == "prod" ]
+then
+    echo "prod environment"
+fi
